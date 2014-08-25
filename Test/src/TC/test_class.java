0e2caa81b8de846949256e2a663499283a136229
sharma.himanshu <sharma.himanshu@L-1QHP412.jasperindia.local>
2014-08-25 10:07:54 +0530

    done

---
+++ Test/src/TC/test_class.java
@@ -0,0 +1,14 @@
+package TC;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class test_class {
+
+	@Test
+	
+	public void test(){
+		System.out.println("Hello Himanshu");
+	}
+}
